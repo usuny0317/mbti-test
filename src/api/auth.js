89a631c65205d99a2b9cf41f212ada84b84ed207
usuny0317 <usuny0317@naver.com>
2025-02-21 20:07:58 +0900

    feat 회원정보 수정하기 기능 완료

--- src/api/auth.js
+++ src/api/auth.js
@@ -19,4 +19,17 @@ export const login = async (userData) => {
 export const getUserProfile = async (token) => {};
 
 //유저 정보 수정하기
-export const updateProfile = async (formData) => {};
+export const updateProfile = async (formData) => {
+  //헤더 만들기
+  const token = localStorage.getItem("accessToken");
+  const headers = {
+    //"Content-Type": "multipart/form-data",
+
+    Authorization: `Bearer ${token}`,
+  };
+
+  const response = await axios.patch(`${API_URL}/profile`, formData, {
+    headers,
+  });
+  return response.data;
+};
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,20 +1,44 @@
+import { useState } from "react";
+import { updateProfile } from "../api/auth";
+
 const Profile = () => {
-  const handleNickChange = () => {
-    alert("수정되었습니다!");
+  const [nickname, setNickname] = useState();
+
+  const handleNickChange = async () => {
+    const newprofile = {
+      nickname: nickname,
+    };
+
+    try {
+      const result = await updateProfile(newprofile);
+      if (result.success) {
+        alert("수정되었습니다!");
+      } else throw "실패!";
+    } catch (err) {
+      alert("수정 실패!" + err);
+    }
+    setNickname("");
   };
   return (
     <div>
       <h1>프로필 페이지</h1>
+
       <label>
-        닉네임이예요!
-        <button
-          onClick={() => {
-            handleNickChange();
+        바꿀 닉네임을 입력해주세요!
+        <input
+          placeholder="닉네임 입력"
+          onChange={(e) => {
+            setNickname(e.target.value);
           }}
-        >
-          닉네임 수정하기
-        </button>
+        />
       </label>
+      <button
+        onClick={() => {
+          handleNickChange();
+        }}
+      >
+        닉네임 수정하기
+      </button>
     </div>
   );
 };
