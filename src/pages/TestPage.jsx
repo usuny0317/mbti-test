401034f47124478e65084ecd987b3e26d590ce6d
usuny0317 <usuny0317@naver.com>
2025-02-25 03:16:36 +0900

    style TestPage 버튼 색 지정 안된 것 수정

--- src/pages/TestPage.jsx
+++ src/pages/TestPage.jsx
@@ -58,7 +58,7 @@ const TestPage = () => {
             </p>
             <button
               onClick={handleNavigateToResults}
-              className="w-full bg-primary-color text-white py-3 rounded-lg font-semibold hover:bg-primary-dark transition duration-300 hover:text-[#FF5A5F]"
+              className="w-full bg-blue-100 text-blue-800 py-3 rounded-lg font-semibold hover:bg-primary-dark transition duration-300 hover:text-blue-400 transition"
             >
               결과 페이지로 이동하기
             </button>
